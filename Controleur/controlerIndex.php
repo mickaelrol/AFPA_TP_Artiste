d2bf82c7c520bb677710aa44986f268276f85aa1
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-05 21:39:36 +0100

    Create controlerIndex.php

---
+++ Controleur/controlerIndex.php
@@ -0,0 +1,21 @@
+<?php
+
+    // require_once 'models/oeuvre.php';
+    require_once 'models/modelExposition.php';
+    require_once 'views/view.php';
+
+    class ControlerIndex{
+
+        private $exposition;
+
+        public function __construct(){
+            $this->exposition = new Exposition();
+        }
+
+        public function index(){
+            $expos = $this->exposition->getLastExpositions();
+            
+            $view = new Vue('Index');
+            $view->render(array('lastExpo'=> $expos));
+        }
+    }
