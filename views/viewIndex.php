65f5ec60526f5c3bf6cab7590b6cd0f04c4e76a5
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 11:50:36 +0100

    Update viewIndex.php

--- views/viewIndex.php
+++ views/viewIndex.php
@@ -1,17 +1,17 @@
 <?php 
 $this->title ='Accueil - Site Artiste';?>
 <section >
-    <div class="col-12 middle">
+    <div>
         <h1>Bonjour , bienvenue sur le site d'arthur le moustachu !</h1>
     </div>
-    <div class="col-12">
-        <img src="Public/Images/auteur.jpg" alt="Arthur le moustachu !">
+    <div>
+        <img src="Public/Images/auteur.jpg" alt="Arthur le moustachu !" id="imgauteur">
     </div>
-    <div class="col-12 mt-5">
-        <h2 class="mb-5">Voici la liste des 3 dernieres expositions :</h2>
+    <div>
+        <h2>Voici la liste des 3 dernieres expositions :</h2>
         <?php
             foreach ($lastExpo as $expo) :
-                echo "<h3 class='middle'>".$expo['nom']."</h3><br>";
+                echo "<h3>".$expo['nom']."</h3>";
                 echo $expo['dateDebut']." au ";
                 echo $expo['dateFin'];
                 echo "<br>";
