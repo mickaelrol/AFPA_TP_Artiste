8748e7ff030cdfbbe36e05b050edf1c6b4337f7a
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 16:50:58 +0100

    Update routeur.php

--- Controleur/routeur.php
+++ Controleur/routeur.php
@@ -2,6 +2,7 @@
     require 'models/model.php';
     require_once 'Controleur/controlerIndex.php';
     require_once 'Controleur/controlerExposition.php';
+    require_once 'Controleur/controlerExpositions.php';
     require_once 'Controleur/controlerOeuvres.php';
     require_once 'Controleur/controlerOeuvre.php';
     
@@ -10,11 +11,13 @@
     class Router {
 
         private $ctrlIndex;
+        private $ctrlExpositions;
         private $ctrlExposition;
         private $ctrlOeuvres;
         private $ctrlOeuvre;
 
         public function __construct(){
+            $this->ctrlExpositions = new ControlerExpositions();
             $this->ctrlExposition = new ControlerExposition();
             $this->ctrlOeuvres = new ControlerOeuvres();
             $this->ctrlOeuvre = new ControlerOeuvre();
@@ -25,7 +28,7 @@
             try{
                 switch ($_GET['action']) {
                     case 'expositions':
-                        $this->ctrlExposition->expositions();
+                        $this->ctrlExpositions->expositions();
                     break;
                     case 'index':
                         $this->ctrlIndex->index();
@@ -34,7 +37,10 @@
                         $this->ctrlOeuvres->oeuvres();
                     break;
                     case 'oeuvre':
-                        $this->ctrlOeuvre->oeuvre();
+                        $this->ctrlOeuvre->oeuvre($_GET['id']);
+                    break;
+                    case 'exposition':
+                        $this->ctrlExposition->exposition($_GET['id']);
                     break;
                     
                     default:
