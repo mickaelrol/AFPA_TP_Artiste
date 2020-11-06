2e15d22431632679f175bb342e29b91bd18071bd
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 16:51:11 +0100

    Update viewOeuvre.php

--- views/viewOeuvre.php
+++ views/viewOeuvre.php
@@ -1,13 +1,11 @@
 <?php 
 $this->title ='Oeuvre - Site Artiste';?>
 <section>
-    <?php
-        foreach ($AllOeuvre as $oeuvre) :?>
             <img src="<?=$oeuvre['grandeImage']?>" alt="<?=$oeuvre['titre']?>">
             <?php 
             echo "<p>Titre : ".$oeuvre['titre']."</p>";
             echo "<p>Année : ".$oeuvre['annee']."</p>";
             echo "<p>Prix : ".$oeuvre['prix']."</p>";
-        endforeach; ?>
+            ?>
     
 </section>
