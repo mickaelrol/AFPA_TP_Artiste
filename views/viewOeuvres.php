5b60fec3ae6c4d4db7ed2d6a0677162b9173613f
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 16:51:13 +0100

    Update viewOeuvres.php

--- views/viewOeuvres.php
+++ views/viewOeuvres.php
@@ -2,12 +2,28 @@
 $this->title ='Oeuvres - Site Artiste';?>
 <section>
     <?php
-        foreach ($AllOeuvres as $oeuvre) :?>
-            <a href='?action=oeuvre&id=<?=$oeuvre['id']?>'><img src="<?=$oeuvre['petiteImage']?>" alt="<?=$oeuvre['titre']?>"></a>
+        foreach ($AllOeuvres as $oeuvre) :
+            echo "<div class='divexpo'>";
+            ?>
+                <a href='?action=oeuvre&id=<?=$oeuvre['id']?>'><img src="<?=$oeuvre['petiteImage']?>" alt="<?=$oeuvre['titre']?>"></a><br> 
+            <?php
+                echo "<div class='contExpo'>";
+                echo "<p>Titre : ".$oeuvre['titre']."</p>";
+                echo "<p>Année : ".$oeuvre['annee']."</p>";
+                echo "<p>Prix : ".$oeuvre['prix']."</p>";
+                echo "</div>";
+            echo "</div>";
+            echo "<hr>";
+
+
+
+
+
+
+        ?>
+            
             <?php 
-            echo "<p>Titre : ".$oeuvre['titre']."</p>";
-            echo "<p>Année : ".$oeuvre['annee']."</p>";
-            echo "<p>Prix : ".$oeuvre['prix']."</p>";
+            
         endforeach; ?>
     
 </section>
