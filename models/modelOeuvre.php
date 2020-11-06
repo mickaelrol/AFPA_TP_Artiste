5ee20f06bf39cd6f7e30b66e4fe56f81ec6aceb2
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 16:51:03 +0100

    Update modelOeuvre.php

--- models/modelOeuvre.php
+++ models/modelOeuvre.php
@@ -1,12 +1,11 @@
 <?php
 
-
     class Oeuvre extends Model {
-        private $id = $_GET['id'];
-        
         public function getOeuvre($id){
             $sql = "SELECT * FROM oeuvre WHERE id  = ?";
             $getOeuvre =$this->goQuery($sql,array($id));
-            return $getOeuvre;
+            $retour = $getOeuvre->fetch();
+            $getOeuvre->closeCursor();
+            return $retour;
         }
     }
