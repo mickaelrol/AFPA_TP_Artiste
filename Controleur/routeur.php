5c186ccc1ca87bcd050f979a8bc0091c1b70ca4d
mickaelrol <mickaelrol.devwwm79@gmail.com>
2020-11-06 11:50:20 +0100

    Update routeur.php

--- Controleur/routeur.php
+++ Controleur/routeur.php
@@ -1,15 +1,23 @@
 <?php
-
+    require 'models/model.php';
     require_once 'Controleur/controlerIndex.php';
     require_once 'Controleur/controlerExposition.php';
+    require_once 'Controleur/controlerOeuvres.php';
+    require_once 'Controleur/controlerOeuvre.php';
+    
+
 
     class Router {
 
         private $ctrlIndex;
         private $ctrlExposition;
+        private $ctrlOeuvres;
+        private $ctrlOeuvre;
 
         public function __construct(){
             $this->ctrlExposition = new ControlerExposition();
+            $this->ctrlOeuvres = new ControlerOeuvres();
+            $this->ctrlOeuvre = new ControlerOeuvre();
             $this->ctrlIndex  = new ControlerIndex();
         }
 
@@ -21,6 +29,12 @@
                     break;
                     case 'index':
                         $this->ctrlIndex->index();
+                    break;
+                    case 'oeuvres':
+                        $this->ctrlOeuvres->oeuvres();
+                    break;
+                    case 'oeuvre':
+                        $this->ctrlOeuvre->oeuvre();
                     break;
                     
                     default:
